bddab30cba48db450df6d6f2621982fa285f7057
Pavel Sobolev <paveloom@tuta.io>
2021-06-21 20:50:31 +0300

    Add an icon to the window.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 // Switch from the console subsystem to the windows subsystem in the release mode
 #![cfg_attr(all(windows, not(debug_assertions)), windows_subsystem = "windows")]
 
-use fltk::{app, prelude::*, window::Window};
+use fltk::{app, image::PngImage, prelude::*, window::Window};
 
 fn main() {
+    let icon: PngImage = PngImage::from_data(include_bytes!("../assets/eiri.png")).unwrap();
     let app = app::App::default();
-    let mut wind = Window::new(100, 100, 400, 300, "Hello from Rust");
+    let mut wind = Window::new(100, 100, 400, 300, "Eiri");
+    wind.set_icon(Some(icon));
     wind.end();
     wind.show();
     app.run().unwrap();
